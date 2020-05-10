fb64932515385b16b2316b970d5aea6d43d688c3
Jean Griesbacher <jnsmplcpt@gmail.com>
2020-05-10 12:18:18 +0200

    Refactor: using webDataBinder for data validation

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -6,23 +6,26 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import javax.validation.Valid;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
 
 @Slf4j
 @Controller
 public class RecipeController {
 
-    private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
+    private static final String RECIPE_RECIPE_FORM_URL = "recipe/recipeform";
     private final RecipeService recipeService;
+
+    private WebDataBinder webDataBinder;
 
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
+    }
+
+    @InitBinder
+    public void initDataBinder(WebDataBinder webDataBinder){
+        this.webDataBinder = webDataBinder;
     }
 
     @GetMapping("/recipe/{id}/show")
@@ -43,19 +46,20 @@ public class RecipeController {
     @GetMapping("recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findCommandById(id).block());
-        return RECIPE_RECIPEFORM_URL;
+        return RECIPE_RECIPE_FORM_URL;
     }
 
     @PostMapping("recipe")
-    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult){
+    public String saveOrUpdate(@ModelAttribute("recipe") RecipeCommand command){
+
+        webDataBinder.validate();
+        BindingResult bindingResult = webDataBinder.getBindingResult();
 
         if(bindingResult.hasErrors()){
 
-            bindingResult.getAllErrors().forEach(objectError -> {
-                log.debug(objectError.toString());
-            });
+            bindingResult.getAllErrors().forEach(objectError -> log.debug(objectError.toString()));
 
-            return RECIPE_RECIPEFORM_URL;
+            return RECIPE_RECIPE_FORM_URL;
         }
 
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command).block();
